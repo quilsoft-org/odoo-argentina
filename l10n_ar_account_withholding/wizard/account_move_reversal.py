592998dd1fa79b1163841afdceacb3aaa8a44f3b
Jorge Obiols <jobiols@users.noreply.github.com>
2021-12-30 17:59:36 -0300

    [FIX] falla al crear una reversion de factura

--- l10n_ar_account_withholding/wizard/account_move_reversal.py
+++ l10n_ar_account_withholding/wizard/account_move_reversal.py
@@ -6,6 +6,15 @@ class AccountMoveReversal(models.TransientModel):
     _inherit = "account.move.reversal"
 
     def reverse_moves(self):
-        """ Forzamos fecha de la factura original para que el amount total de la linea se calcule bien"""
-        self = self.with_context(invoice_date=self.move_id.date)
+        """ Forzamos fecha de la factura original para que el amount total de la linea
+            se calcule bien
+        """
+
+        # TODO Hay que hacer QA con esto porque no se que implicaciones puede tener
+        # en la v13 es self.move_id con lo que hay solo una factura y una fecha, aqui
+        # manejan un conjunto de facturas. Estoy tomando la primera solamente para ver
+        # que pasa pero no se que puede pasar si vienen varias facturas.
+
+        date = self.move_ids[0].date
+        self = self.with_context(invoice_date=date)
         return super(AccountMoveReversal, self).reverse_moves()
